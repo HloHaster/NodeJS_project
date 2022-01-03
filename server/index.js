44ee3a9e328e11e0b28586df6f6611bb911e3733
Tanya Hlopenkova <tanyahlopenkova@gmail.com>
2022-01-04 00:41:10 +0300

    make client navbar

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const mongodb = require('mongodb')
 
 const postsRoutes = require('./routes/posts')
 const categoriesRoutes = require('./routes/categories')
